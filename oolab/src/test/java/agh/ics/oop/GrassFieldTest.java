c9ba399a12552a98db5126beb0113b9b8b88fd92
MiZuii <piotr.wiercigroch1@gmail.com>
2022-11-20 02:17:22 +0100

    lab5 tests update

--- oolab/src/test/java/agh/ics/oop/GrassFieldTest.java
+++ oolab/src/test/java/agh/ics/oop/GrassFieldTest.java
@@ -3,7 +3,6 @@ package agh.ics.oop;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Iterator;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,29 +12,25 @@ class GrassFieldTest {
     GrassField map;
     int mapLength;
     int n;
-    Vector2d[] grassPlaces;
-    Vector2d[] animalPlaces;
+    Vector2d[] grassPositions;
+    Vector2d[] animalPositions;
+    Vector2d[] emptyPositions;
+    Random randomizer;
 
-    boolean[] elementPlaces;
 
     @BeforeEach
     void setUp() {
-        Random randomizer = new Random();
+        randomizer = new Random();
         n = randomizer.nextInt(20)+10;
         map = new GrassField(n);
         mapLength = (int)Math.sqrt(10*n);
-        elementPlaces = new boolean[4+n];
 
         // placeing animals
         map.place(new Animal(map, new Vector2d(0, 0)));
-        elementPlaces[0] = true;
         map.place(new Animal(map, new Vector2d(0, mapLength/2)));
-        elementPlaces[mapLength*n/2] = true;
         map.place(new Animal(map, new Vector2d(mapLength/3, 0)));
-        elementPlaces[mapLength/3] = true;
         map.place(new Animal(map, new Vector2d(mapLength-5, mapLength-7)));
-        elementPlaces[(mapLength-7)*n + mapLength - 5] = true;
-        animalPlaces = new Vector2d[]{
+        animalPositions = new Vector2d[]{
                 new Vector2d(0,0),
                 new Vector2d(0, mapLength/2),
                 new Vector2d(mapLength/3, 0),
@@ -43,13 +38,18 @@ class GrassFieldTest {
         };
 
         // creating array of grass placement
-        grassPlaces = new Vector2d[n];
+        grassPositions = new Vector2d[n];
+        emptyPositions = new Vector2d[mapLength*mapLength-n-4];
         int grassIter = 0;
+        int emptyIter = 0;
         for(int row=0; row<mapLength; row++) {
             for (int column=0; column<mapLength; column++) {
-                if(map.objectAt(new Vector2d(row, column)) instanceof Grass) {
-                    grassPlaces[grassIter++] = new Vector2d(row, column);
-                    elementPlaces[row*n+column] = true;
+                Vector2d rcVector = new Vector2d(row, column);
+                if(map.objectAt(rcVector) instanceof Grass) {
+                    grassPositions[grassIter++] = rcVector;
+                }
+                else if(map.objectAt(rcVector) == null) {
+                    emptyPositions[emptyIter++] = rcVector;
                 }
             }
         }
@@ -57,18 +57,64 @@ class GrassFieldTest {
 
     @Test
     void canMoveTo() {
+        // all animal places are unable to be moved to
+        for(Vector2d position : animalPositions) {
+            assertFalse(map.canMoveTo(position));
+        }
+
+        // all grass places should be possible to be moved to
+        for(Vector2d position : grassPositions) {
+            assertTrue(map.canMoveTo(position));
+        }
+
+        // all empty places should be possible to be moved to
+        for(Vector2d position : emptyPositions) {
+            assertTrue(map.canMoveTo(position));
+        }
     }
 
     @Test
     void objectAt() {
+        // check all animal places
+        for(Vector2d position : animalPositions) {
+            assertTrue(map.objectAt(position) instanceof Animal);
+        }
+
+        // check all grass places
+        for(Vector2d position : grassPositions) {
+            assertTrue(map.objectAt(position) instanceof Grass);
+        }
+
+        // check all empty places
+        for(Vector2d position : emptyPositions) {
+            assertNull(map.objectAt(position));
+        }
     }
 
     @Test
     void place() {
+        // check if animals can't be placed on other animals
+        for(Vector2d position : animalPositions) {
+            assertFalse(map.place(new Animal(map, position)));
+        }
     }
 
     @Test
     void isOccupied() {
+        // check all animal places
+        for(Vector2d position : animalPositions) {
+            assertTrue(map.isOccupied(position));
+        }
+
+        // check all grass places
+        for(Vector2d position : grassPositions) {
+            assertTrue(map.isOccupied(position));
+        }
+
+        // check all empty places
+        for(Vector2d position : emptyPositions) {
+            assertFalse(map.isOccupied(position));
+        }
     }
 
     @Test
